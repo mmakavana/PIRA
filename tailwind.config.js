d31ff2c252c09a41920cb57646d328265c2d5e5f
mmakavana <mmakavana@gmail.com>
2025-09-19 16:05:23 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Candara', 'Inter', 'ui-sans-serif', 'system-ui'],
+      colors: {
+        pira: {
+          purple: "#2d2671",
+          purple50: "#f3f2ff",
+          purple100: "#e8e6ff",
+          purple600: "#4b44a3"
+        }
       },
+      borderRadius: {
+        'xl': '14px',
+        '2xl': '18px'
+      }
     },
   },
   plugins: [],
-};
+}
